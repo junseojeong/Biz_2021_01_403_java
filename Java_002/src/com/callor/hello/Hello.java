33a7d13c49980efb10066e23a9bf9cf4b871b4cf
junseojeong <jsjeong96@gmail.com>
2021-02-02 14:29:18 +0900

    명령어

---
+++ Java_002/src/com/callor/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.callor.hello;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println(30 + 40);
+	System.out.println("30 x 40 ="+ 30 * 40);
+	System.out.println(40 - 30);
+	System.out.println(30 * 40);
+}
+}
